d10081d1cce66da3d03816c3d84e00fc081002d0
mega-maruthu <mega@nexinfinitymeta.ai>
2026-05-29 13:00:39 +0530

    Feat: Create Task model and run initial migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
+
 from django.db import models
 
-# Create your models here.
+# We are using Python Classes here (Python Basics)
+class Task(models.Model):
+    # These variables define the columns in your PostgreSQL table
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # This Python method tells Django how to print this object as text
+    def __str__(self):
+        return self.title
